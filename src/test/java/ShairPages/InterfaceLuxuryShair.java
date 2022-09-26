16c3d8b06615fd76df534b8dd4882e394a804e15
AunjunH <mizanheera@gmail.com>
2022-09-25 19:22:39 -0500

    Added Shair.co luxoryCar interface class

---
+++ src/test/java/ShairPages/InterfaceLuxuryShair.java
@@ -0,0 +1,14 @@
+package ShairPages;
+
+public interface InterfaceLuxuryShair {
+    int timeFrame=0;
+    void apply(int decrement);
+    void setTimeFrame(int increment);
+}
+interface Ebike{
+    void RentTime();
+    Void daysCount();
+}
+interface SuperLuxory{
+
+}
